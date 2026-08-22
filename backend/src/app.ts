67934ba228ca8e6ce1032f8585afeed3d1db429c
Anushtha Singh Kushwah <anushthasinghk@gmail.com>
2026-08-22 20:54:45 +0530

    add centralized error handling

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import healthRoutes from './routes/health-routes'
+import { errorHandler } from './middleware/error-middleware';
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/api/health", healthRoutes);
+
+app.use(errorHandler);
 
 export default app;
